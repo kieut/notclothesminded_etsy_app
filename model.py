5f0d676df6058cbad0ae8ea073e9df729ecb2771
Kieu Tran <kieudung.tran@gmail.com>
2014-07-11 18:16:04 -0700

    seed.py writes to db, but materials is currently stored as a joined string instead of a list

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, Text, Float
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, backref
@@ -9,9 +9,12 @@ engine = create_engine("sqlite:///notclothesminded.db", echo=False)
 db_session = scoped_session(sessionmaker(bind=engine,
                                       autocommit = False,
                                       autoflush = False))
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 Base.query = db_session.query_property()
+
 ### Class declarations go here
 class User(Base):
     __tablename__ = "users"
@@ -24,53 +27,62 @@ class User(Base):
     age = Column(Integer, nullable=True)
     zipcode = Column(String(15), nullable=True)
 
-    # ratings = relationship("Ratings")
 
 class Listing(Base):
     __tablename__= "listings"
 
     id = Column(Integer, primary_key = True)
-    title = Column(String(64), nullable = True)
-    description = Column(Text(???), nullable = True) # LOOK AT THIS
+    title = Column(String(140), nullable = True)
+    description = Column(Text, nullable = True) # LOOK AT THIS, string or text data type
     listing_url = Column(String(100), nullable = False)
-    price = Column(Float, nullable = False)
-    shop_section_id = Column(Integer, nullable = True) # shop id can return null?
+    price = Column(String(64), nullable = False)
     materials = Column(String(100), nullable = True)
-    color = Column(String(64), nullable = True)
+    # color is in the params key
+    # color = Column(String(64), nullable = True)
     currency = Column(String(64), nullable = False)
     bust = Column(Integer, nullable = True)
     waist = Column(Integer, nullable = True)
     hip = Column(Integer, nullable = True)
     natural_waist = Column(Integer, nullable = True)
-    length = Column(Integer, nullable = True)
+    dress_length = Column(Integer, nullable = True)
+    skirt_length = Column(Integer, nullable = True)
+    shirt_length = Column(Integer, nullable = True)
     arm_length = Column(Integer, nullable = True)
-
-    """look up datetime and epoch time"""
-    last_crawl = Column(DateTime, nullable = True)
-    last_modified = Column(Epoch) # look up datatype for epoch
-
-
-
-    # ratings = relationship("Ratings")
+    creation_date = Column(Integer, nullable = True)
+    state = Column(String(64), nullable = True)
+    last_crawl = Column(Integer, nullable = True) # get epoch time from python, utc, save as int
+    last_modified = Column(Integer, nullable = False) # already displayed in epoch time, just save as an int
 
 class UserFavorite(Base):
     __tablename__ = "favorites"
 
     id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable = True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable = True)
     #ForeignKey says it references another column in another table
-    listing_id = Column(Integer, ForeignKey('listings.id'), nullable = True)
+    listing_id = Column(Integer, ForeignKey("listings.id"), nullable = True)
 
-    #This establish relationship between Rating and User objects with 'backref'
     user = relationship("User", backref=backref("favorites", order_by=id))
     listing = relationship("Listing", backref=backref("favorites", order_by=id))
 
-# ## End class declarations
+    
+class ListingImage(Base):
+    __tablename__= "images"
+
+    id = Column(Integer, primary_key = True)
+    listing_image_id = Column(Integer(64), nullable = False)
+    listing_id = Column(Integer, ForeignKey("listings.id"), nullable = False)
+    url_170x135 = Column(String(200), nullable = True)
+    url_570xN = Column(String(200), nullable = True)
+    url_75x75 = Column(String(200), nullable = True)
+    url_fullxfull = Column(String(200), nullable = True)
+
+    listing = relationship("Listing", backref=backref("listings", order_by=id))
+
 # def connect():
-#     global ENGINE 
+#     global ENGINE
 #     global Session
 
-#     ENGINE = create_engine("sqlite:///ratings.db", echo=False)
+#     ENGINE = create_engine("sqlite:///notclothesminded.db", echo=True)
 #     Session = sessionmaker(bind=ENGINE)
 
 #     return Session()
--- seed.py
+++ seed.py
@@ -1,55 +1,105 @@
-import model
+# import model
 import json, httplib2
 import pprint
 import urllib
+from model import db_session
+import model
 
 mykey = 'vgmfi5108akc4ot4rthtrw08'
 
 
-# o this is *basically* allowing me to do a remote table query on the etsy database
-# so I'm basically interacting with the etsy database through their api, which is a http request?
-# Now how do I do it in webapp.py???
-
-def connect(mykey):
+# pass 'offset' variable to this from main function, figure out where it's left off every time you run function.
+def get_listings(mykey):
 	#HTTP GET request for active listings, filter by category string path "Vintage/Clothing"
-	#Need to figure out how to paginate through count?
-	#everything after ? is a parameter
+	#Need to loop through paginations.
+	#everything after '?'' is a parameter
 	#urllib creates the url with parameters for me
 
-
-	"""Need to convert this to urlli"""
-	# for p in range(10, 100):
-	# 	resp, content = httplib2.Http().request("https://openapi.etsy.com/v2/listings/active?api_key=vgmfi5108akc4ot4rthtrw08&limit=5&offset=0&category=Vintage%%2FClothing&min_price=%d&max_price=%d" % (p, p+1))
-	# 	print p, json.loads(content)['count']
-
-	p = 0
 	"""Need a new url for each time through this loop, so need to do the urlencode inside the loop (and the join)"""
 	"""This only gets me the count for each 'price bucket', only gives me 100 items per query.
 	Still need to query for items for 100-150, 150-200, and >200, and tab through paginations"""
-	for p in range(10, 100):
+	# need to put this is another for loop, to set offset to i+100
+	p = 0
+
+	for p in range(100, 101):
 		parameters = urllib.urlencode({'api_key': mykey, 'limit': 100, 'offset': 0, 'category': 'Vintage/Clothing', 'min_price': p, 'max_price': p+1})
 		url = '?'.join(['https://openapi.etsy.com/v2/listings/active', parameters])
-		# print "url__________________%s" % url
 		resp, content = httplib2.Http().request(url)
-		print p, json.loads(content)['count']
+		# print p, json.loads(content)['count']
+		result = json.loads(content)
+	return result # dict of all objects
 
+def get_images(result):
+	"""There wasn't a need for creating a listing_id_list to loop through first,
+	already has access to the listing_id's through results"""
+	results = result['results'] # list of objects
+	length = len(results)
 
+	for i in range(length):
+		listing_id = results[i]['listing_id']
+		parameters = urllib.urlencode({'api_key': 'vgmfi5108akc4ot4rthtrw08', 'limit': 100, 'offset': 0, 'listing_id': i})
+		url = '?'.join(['https://openapi.etsy.com/v2/listings/%d/images' % listing_id, parameters])
+		resp, content = httplib2.Http().request(url)
+		images = json.loads(content)
+	return images # dict
 
-
-
-	# resp, content = httplib2.Http().request("https://openapi.etsy.com/v2/listings/active?api_key=vgmfi5108akc4ot4rthtrw08&limit=5&offset=0&category=Vintage/Clothing")
-	# result = json.loads(content)
-	# pprint.pprint(result['results'])
-	# length = len((result['results']))
-
-	# print "_______________length:%r" % length
-
-
-def search_results():
+def get_bust(result): 
+	"""parse description for measurements"""
+	#.*Bust\s*[ :-]?\D*([0-9]+)
 	pass
 
-def main():
-	connect(mykey)
+def get_waist(result):
+	pass
+
+def get_hip(result):
+	pass
+
+def get_natural_waist(result):
+	pass
+
+def get_dress_length(result):
+	pass
+
+"""How will I know if it's a skirt, I think I need to build out my
+data model and possibly split listings into diff types of clothing"""
+def get_skirt_length(result):
+	pass
+
+def get_shirt_length(result):
+	pass
+
+def get_arm_length(result):
+	pass
+
+ 
+def main(db_session):
+	"""Map objects to database from here so can move functions around"""
+	result = get_listings(mykey)
+	results = result['results'] # this is a list
+
+#for every listing in results is an object with attributes
+	for i in results:
+		# pprint.pprint(listing)
+
+		listing = model.Listing(title = i['title'], description = i['description'],
+			listing_url = i['url'], price = str(i['price']),
+			materials = ",".join(i['materials']), currency = i['currency_code'], creation_date = i['creation_tsz'], state = i['state'],
+			last_modified = i['last_modified_tsz'])
+		db_session.add(listing)
+
+
+	images = get_images(result) #dict??
+	images_list = images['results'] # list
+
+	for i in images_list:
+		# pprint.pprint(image)
+		image = model.ListingImage(listing_image_id = i['listing_image_id'], listing_id = i['listing_id'],
+			url_170x135 = i['url_170x135'], url_570xN = i['url_570xN'], url_75x75 = i['url_75x75'],
+			url_fullxfull = i['url_fullxfull'])
+		db_session.add(image)
+
+	db_session.commit()
 
 if __name__ == "__main__":
-    main()
+    s= model.db_session()
+    main(s)
